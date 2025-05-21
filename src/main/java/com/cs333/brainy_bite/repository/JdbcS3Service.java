d3168c69a88c1acd6d8a420ccf255c3cafca3eaf
Thara Sritharadol <sunsummerone1@gmail.com>
2025-05-21 13:39:33 +0700

    Modify Add Article

--- src/main/java/com/cs333/brainy_bite/repository/JdbcS3Service.java
+++ src/main/java/com/cs333/brainy_bite/repository/JdbcS3Service.java
@@ -1,5 +1,6 @@
 package com.cs333.brainy_bite.repository;
 
+import com.cs333.brainy_bite.model.articles;
 import com.cs333.brainy_bite.payload.request.PendingRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -23,6 +24,19 @@ public class JdbcS3Service implements S3Repository{
         } catch (DataAccessException e) {
             e.printStackTrace();
             throw new RuntimeException("Database insert failed", e);  // หรือจัดการอย่างเหมาะสม
+        }
+    }
+
+    public int insertArticle(articles articles, String mk_url, String thumbnail_url) {
+        try {
+            return jdbcTemplate.update("INSERT INTO articles (published, author, category, title, description, " +
+                    "article_url, thumbnail_url)\n" +
+                    "VALUE (CURRENT_DATE(), ?, ?, ?, ?, ?, ?)",
+                    new Object[] {articles.getAuthor(), articles.getCategory(), articles.getTitle(), articles.getDescription(),
+                    articles.getArticle_url(), articles.getThumbnail_url()});
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Database insert failed", e);
         }
     }
 }
